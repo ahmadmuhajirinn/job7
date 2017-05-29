4ed078ed7b0a7f53cd34747a1e69fee20bf34838
ahmadmuhajirin <ahmadmuajirin86@gmail.com>
2017-05-29 23:13:41 +0700

    Add files via upload

---
+++ job7.php
@@ -0,0 +1,19 @@
+
+<html>
+<head>
+<title>contoh pemakaian session <title>
+<head>
+<body>
+<?php>
+<p>
+<pre>
+Silahkan Login
+ User :<input type="text"name="user"Value="">
+ Sandi:<input type="password"name="sandi"value=>  
+    <input type="submit"name="login"vaue=">
+</pre>
+</p>
+</form>
+<?php>
+</html>
+
